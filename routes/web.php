6b1c3f39483b1c445d3ecd9caa7dea376017d109
Kelly <Kelly.duran@cajamag.com.co>
2023-03-08 10:54:36 -0500

    Se agregaron las rutas para el login y el logout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,13 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::post('/register', [RegisterController::class, 'store']);
+
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::get('/logout', [LogoutController::class, 'store'])->name('logout');
+
 
 
 Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
